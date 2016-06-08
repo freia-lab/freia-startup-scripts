6b2fbb37a99923c8eebb0ecbbdb2af4f9abc857c
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2016-06-08 17:11:10 +0200

    Added TZ variable to ioc08-misc. Updated Epics env to 1.8.0

--- ioc/nuc-01/ioc08-misc/env.sh
+++ ioc/nuc-01/ioc08-misc/env.sh
@@ -3,7 +3,8 @@ BASE=3.14.12.5
 EPICS_BASE=/opt/epics/bases/base-3.14.12.5
 EPICS_HOST_ARCH=centos7-x86_64
 EPICS_MODULES_PATH=/opt/epics/modules
-ENVIRONMENT_VERSION=1.7.2
+ENVIRONMENT_VERSION=1.8.0
 #EPICS_CA_ADDR_LIST=""
+TZ='Europe/Stockholm'
 # Specific configuration
 PROCSERV_PORT=2008
