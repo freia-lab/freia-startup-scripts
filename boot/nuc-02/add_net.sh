e9a169a39db93e8641810f3a7003256647a805df
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2016-06-10 16:09:48 +0200

    Added Epics CA gateway on nuc-02

--- boot/nuc-02/add_net.sh
+++ boot/nuc-02/add_net.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
 echo "Stopping LDAP client daemon"
 systemctl stop nslcd
 nmcli device show >/tmp/network_devices
+hostname >/tmp/hostname
 #nmcli connection add type ethernet con-name PLC1 ifname enp0s20u4 ip4 10.10.48.97/24
-nmcli connection add type ethernet con-name PLC1 ifname enp0s20u4 ip4 192.168.1.116/24
-nmcli connection up PLC1
-ip route add default via 192.168.10.1 dev eno1
-#touch /tmp/konrad
+echo "Adding CAGWPUB connection"
+nmcli connection add type ethernet con-name CAGWPUB ifname enp0s20u3 ip4 130.238.200.140/25
+echo "Setting up CAGWPUB connection"
+nmcli connection up CAGWPUB
+#ip route add default via 192.168.10.1 dev eno1
+touch /tmp/nuc-02
---
+++ boot/nuc-02/gateway/start_gateway.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+host=`hostname`
+gwhome=/var/tmp/gateway
+uid=997
+gid=995
+
+export PATH=/opt/epics/bases/base-3.14.12.5/bin/centos7-x86_64:/opt/epics/modules/environment/1.6.0/3.14.12.5/bin/centos7-x86_64:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
+export EPICS_HOST_ARCH=centos7-x86_64
+export EPICS_MODULES_PATH=/opt/epics/modules
+export EPICS_ENV_PATH=/opt/epics/modules/environment/1.6.0/3.14.12.5/bin/centos7-x86_64
+export EPICS_BASES_PATH=/opt/epics/bases
+ 
+start()
+{
+	echo "Starting CA gateway"
+	[ ! -d $gwhome ] && mkdir $gwhome
+	chown -R $uid:$gid $gwhome
+	EPICS_CA_MAX_ARRAY_BYTES=1600000 gateway -home $gwhome -log gateway.log -prefix GW-$host -cip 192.168.10.255 -sip 130.238.200.140 -archive -no_cache -pvlist /opt/startup/boot/$host/gateway/GATEWAY.pvlist -access /opt/startup/boot/$host/gateway/GATEWAY.access -signore $host -uid $uid -gid $gid -server
+}
+
+stop()
+{
+        echo "Stopping CA gateway"
+        [ -f $gwhome/gateway.killer ] && $gwhome/gateway.killer
+}
+
+
+case "$1" in
+'start')
+  start
+  ;;
+
+'stop')
+  stop
+  ;;
+
+*)
+  echo "Usage: $0 {start|stop}"
+  exit 1
+  ;;
+esac
+
