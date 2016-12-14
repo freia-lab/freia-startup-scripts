48cb1353063d8c9fe20b3f2ddf4b4c5e29903de3
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2016-12-14 11:54:52 +0100

    Added nuc-03 for the tests of esys station on a separate subnet.

--- boot/nuc-03/add_net.sh
+++ boot/nuc-03/add_net.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-
+host=`hostname`
 echo "Stopping LDAP client daemon"
 systemctl stop nslcd
 nmcli device show >/tmp/network_devices
-#nmcli connection add type ethernet con-name PLC1 ifname enp0s20u4 ip4 10.10.48.97/24
-nmcli connection add type ethernet con-name PLC1 ifname enp0s20u4 ip4 192.168.1.117/24
-nmcli connection up PLC1
-ip route add default via 192.168.10.1 dev eno1
-#touch /tmp/konrad
+hostname >/tmp/hostname
+echo "Adding ESYS connection"
+nmcli connection add type ethernet con-name ESYS ifname enp0s20u4 ip4 192.168.4.117/24
+echo "Setting connection ESYS up"
+nmcli connection up ESYS
+echo "Setting up symmetric routing"
+ip route add 192.168.10.0/24 dev eno1 tab 1
+ip route add 192.168.4.0/24 dev enp0s20u4 tab 2
+#ip route add default via 192.168.4.117 dev enp0s20u4 tab 2
+ip route add default via 192.168.10.1 dev eno1 tab 1
+ip rule add from 192.168.10.117/32 tab 1 priority 100
+ip rule add from 192.168.4.117/32 tab 2 priority 200
+ip route flush cache
+
+#ip route add default via 192.168.10.1 dev eno1
---
+++ ioc/nuc-03/ioc05-esys/st.cmd
@@ -0,0 +1,3 @@
+require ioc_esys_freia,konrad
+#require ioc_esys_freia
+requireSnippet(esys-freia.cmd, "HOST_IPADDR=192.168.4.40")
