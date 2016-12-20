a0d32cb62cb509d4801f8978ab65a69ec823c156
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2016-12-20 09:06:44 +0100

    Put Esys RF on a separate subnet. Upgraded to epics 3.15.4.

--- boot/nuc-01/add_net.sh
+++ boot/nuc-01/add_net.sh
@@ -5,6 +5,8 @@ systemctl stop nslcd
 nmcli device show >/tmp/network_devices
 #nmcli connection add type ethernet con-name PLC1 ifname enp0s20u4 ip4 10.10.48.97/24
 nmcli connection add type ethernet con-name PLC1 ifname enp0s20u4 ip4 192.168.1.115/24
+nmcli connection add type ethernet con-name ESYS ifname enp0s20u3 ip4 192.168.4.115/24
 nmcli connection up PLC1
+nmcli connection up ESYS
 ip route add default via 192.168.10.1 dev eno1
 #touch /tmp/konrad
--- ioc/nuc-01/ioc05-esys/st.cmd
+++ ioc/nuc-01/ioc05-esys/st.cmd
@@ -1,3 +1,3 @@
 #require ioc_esys_freia,konrad
 require ioc_esys_freia
-requireSnippet(esys-freia.cmd, "HOST_IPADDR=192.168.10.40")
+requireSnippet(esys-freia.cmd, "HOST_IPADDR=192.168.4.40")
