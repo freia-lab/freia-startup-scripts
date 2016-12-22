0cab1ae2c7ac828c3b1f37c70ebcee56cc9c968c
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2016-12-22 09:20:12 +0100

    All iocs updated to epics 3.15.4 and EEE 1.8.2

--- ioc/nuc-01/ioc02-linde/env.sh
+++ ioc/nuc-01/ioc02-linde/env.sh
@@ -1,8 +1,8 @@
 # General configuration
-BASE=3.14.12.5
-EPICS_BASE=/opt/epics/bases/base-3.14.12.5
+BASE=3.15.4
+EPICS_BASE=/opt/epics/bases/base-3.15.4
 EPICS_HOST_ARCH=centos7-x86_64
 EPICS_MODULES_PATH=/opt/epics/modules
-ENVIRONMENT_VERSION=1.7.0
+ENVIRONMENT_VERSION=1.8.2
 # Specific configuration
 PROCSERV_PORT=2002
