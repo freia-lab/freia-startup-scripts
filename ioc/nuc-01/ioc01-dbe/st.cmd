3b77aa3af29bc87c18d0a08f0c25fa6f73e94b51
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2017-06-07 14:11:30 +0200

    Updated ioc-dbe-freia to EEE v2.0.0.

--- ioc/nuc-01/ioc01-dbe/st.cmd
+++ ioc/nuc-01/ioc01-dbe/st.cmd
@@ -1,3 +1,3 @@
 #require ioc-dbe-freia,konrad
-require ioc-dbe-freia
+require ioc-dbe-freia,1.3+
 requireSnippet(dbe-sample.cmd,"PLC1_IPADDR=192.168.1.248,PLC1_PORT=2000, PLC2_IPADDR=192.168.1.247,PLC2_PORT=2000")
