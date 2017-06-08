b297f0830640184e6e7d79854c292a2790ad15ea
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2017-06-08 17:05:22 +0200

    Updated ioc-superv-freia to EEE v2.0.0.

--- ioc/nuc-01/ioc09-superv/env.sh
+++ ioc/nuc-01/ioc09-superv/env.sh
@@ -3,6 +3,6 @@ BASE=3.15.4
 EPICS_BASE=/opt/epics/bases/base-3.15.4
 EPICS_HOST_ARCH=centos7-x86_64
 EPICS_MODULES_PATH=/opt/epics/modules
-ENVIRONMENT_VERSION=1.8.2
+ENVIRONMENT_VERSION=2.0.0
 # Specific configuration
 PROCSERV_PORT=2009
