6a2047a413e87fec79ac088202eb22645757825e
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2017-08-16 16:00:07 +0200

    Added the first production version of RF leakage detector ioc.

--- ioc/nuc-01/ioc10-rfleak/st.cmd
+++ ioc/nuc-01/ioc10-rfleak/st.cmd
@@ -1,11 +1,14 @@
 #require rfleakdet-freia,konrad
-require rfleakdet-freia,0.1+
+require rfleakdet-freia,1.0+
 epicsEnvSet("RFLEAK_PORT", "RFLEAKDET")
 
 drvAsynIPPortConfigure(${RFLEAK_PORT}, "192.168.10.9:4002",0,0,0)
 #drvAsynIPPortConfigure("PORT", "192.168.10.9:4004",0,0,0)
 
+dbLoadRecords("asynRecord.db","P='Env-RFleakage',R=:asynRec,PORT='${RFLEAK_PORT}',ADDR='0',IMAX='1024',OMAX='256'")
 ## Load record instances
-dbLoadRecords("rfleakdet-main.db","RFDET_PORT='${RFLEAK_PORT}',PREFIX=''")
-dbLoadRecords("rfleakdet.db","RFDET_PORT='${RFLEAK_PORT}',PREFIX='',NODE=0")
-
+dbLoadRecords("rfleakdet-main.db","RFDET_PORT='${RFLEAK_PORT}',PREFIX='Env-RFleakage'")
+dbLoadRecords("rfleakdet.db","RFDET_PORT='${RFLEAK_PORT}',PREFIX='Env-RFleakage',NODE=0")
+dbLoadRecords("rfleakdet.db","RFDET_PORT='${RFLEAK_PORT}',PREFIX='Env-RFleakage',NODE=1")
+dbLoadRecords("rfleakdet.db","RFDET_PORT='${RFLEAK_PORT}',PREFIX='Env-RFleakage',NODE=2")
+dbLoadRecords("rfleakdet.db","RFDET_PORT='${RFLEAK_PORT}',PREFIX='Env-RFleakage',NODE=3")
