08d6f7bc458e77b8f449de821d2caecf5dc7306d
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2017-08-23 14:51:32 +0200

    Updated the startup script for ioc04-wtrvac.

--- ioc/nuc-01/ioc04-wtrvac/st.cmd
+++ ioc/nuc-01/ioc04-wtrvac/st.cmd
@@ -1,3 +1,7 @@
 #require ioc_wtrvac_freia,konrad
-require ioc_wtrvac_freia,1.9+
+require ioc_wtrvac_freia,1.10+
 requireSnippet(wtrvac-freia.cmd, "PLC_IPADDR=192.168.10.10")
+dbpf WtrC-Dwtr:Kly-FT01:sRdV.DESC "Cooling water flow: collector + RF"
+dbpf WtrC-Dwtr:Kly-FT02:sRdV.DESC "Cooling water flow: oil tank + window"
+dbpf WtrC-Dwtr:Kly-FT03:sRdV.DESC "Cooling water flow: klystron body"
+dbpf WtrC-Dwtr:Kly-FT04:sRdV.DESC "Cooling water flow: coil"
