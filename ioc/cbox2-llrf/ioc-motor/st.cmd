07a4f88c0916d29f6a4229af24eebc6299a71b8e
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2017-09-07 10:51:46 +0200

    Added scripts for the second LLRF MTCA system: cbox2-llrf

---
+++ ioc/cbox2-llrf/ioc-motor/st.cmd
@@ -0,0 +1,4 @@
+require cavitytuner,0.2.3
+requireSnippet("freia.cmd")
+dbpf FREIAPMAC:MTR1.VMAX 720
+dbpf FREIAPMAC:MTR1.VELO 720
---
+++ ioc/cbox2-llrf/ioc01/st.cmd
@@ -0,0 +1,4 @@
+#require sis8300llrf
+require ioc-sis8300llrf-freia,0.6.2
+#require ioc-sis8300llrf-freia,0.6.1
+requireSnippet(sis8300llrf-freia.cmd, "SIS8300LLRF_SLOT=4, SIS8300LLRF_PREFIX=LLRF-2, TRLLRF_PREFIX=TR-2")
