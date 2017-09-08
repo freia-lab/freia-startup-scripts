2ae7e6b9741f4e9bbcadac10c9a87586d4bc5787
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2017-09-08 15:43:42 +0200

    Prepared cbox2-llrf ioc01 for base 3.15.4

--- ioc/cbox2-llrf/ioc01/env.sh
+++ ioc/cbox2-llrf/ioc01/env.sh
@@ -1,11 +1,12 @@
 # General configuration
-BASE=3.14.12.5
-#BASE=3.15.4
-EPICS_BASE=/opt/epics/bases/base-3.14.12.5
-#EPICS_BASE=/opt/epics/bases/base-3.15.4
+#BASE=3.14.12.5
+BASE=3.15.4
+#EPICS_BASE=/opt/epics/bases/base-3.14.12.5
+EPICS_BASE=/opt/epics/bases/base-3.15.4
 EPICS_HOST_ARCH=centos7-x86_64
 EPICS_MODULES_PATH=/opt/epics/modules
 #ENVIRONMENT_VERSION=1.8.0
 ENVIRONMENT_VERSION=2.0.0
+EPICS_CAS_INTF_ADDR_LIST="192.168.10.111"
 # Specific configuration
 PROCSERV_PORT=2000
