4f55d6de35803edb5bd8686d03e3f93a2e5d0f06
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2017-11-10 10:55:08 +0100

    New version of cbox2-llrf.

--- ioc/cbox2-llrf/ioc01/st.cmd
+++ ioc/cbox2-llrf/ioc01/st.cmd
@@ -1,4 +1,4 @@
 #require sis8300llrf
-require ioc-sis8300llrf-freia,0.7+
+require ioc-sis8300llrf-freia,0.8+
 #require ioc-sis8300llrf-freia,konrad
 requireSnippet(sis8300llrf-freia.cmd, "SIS8300LLRF_SLOT=4, SIS8300LLRF_PREFIX=LLRF-2, TRLLRF_PREFIX=TR-2")
