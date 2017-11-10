c026d846981e68778f89b93d657270a2c54f7e2c
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2017-11-10 11:11:41 +0100

    Updated cbox-llrf to EEE v2.0.0.

--- ioc/cbox-llrf/ioc01/st.cmd
+++ ioc/cbox-llrf/ioc01/st.cmd
@@ -1,19 +1,4 @@
+#require sis8300llrf,3.3.2
 #require ioc-sis8300llrf-freia,konrad
-#requireSnippet(sis8300llrf-freia.cmd, "SIS8300LLRF_SLOT=4")
-# Tested calibration procedure on Jan, 29th (ok)
-#require sis8300llrf,2.18.7
-#requireSnippet(sis8300llrf-demo.cmd, "SIS8300LLRF_SLOT=4")
-
-# Worked ok up till 25-Feb
-#require sis8300llrf-demo,1.0.0
-#requireSnippet(sis8300llrf-demo-start.cmd, "SIS8300LLRF_SLOT=4")
-
-#Worked ok on 26-Feb
-#require ioc-sis8300llrf-freia,0.1.1
-#requireSnippet(sis8300llrf-demo-start.cmd, "SIS8300LLRF_SLOT=4")
-
-#require sis8300llrf,3.2.0
-require sis8300llrf
-#require ioc-sis8300llrf-freia,konrad
-require ioc-sis8300llrf-freia
-requireSnippet(sis8300llrf-freia.cmd, "SIS8300LLRF_SLOT=4")
+require ioc-sis8300llrf-freia,0.8+
+requireSnippet(sis8300llrf-freia.cmd, "SIS8300LLRF_SLOT=4, SIS8300LLRF_PREFIX=LLRF, TRLLRF_PREFIX=LLRF-TR")
