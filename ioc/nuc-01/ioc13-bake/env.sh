6459a498b8823856e964473ec6dd737aaf0f0f96
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2018-03-27 09:21:20 +0200

    Added ioc13-bake.

---
+++ ioc/nuc-01/ioc13-bake/env.sh
@@ -0,0 +1,11 @@
+# General configuration
+BASE=3.15.4
+EPICS_BASE=/opt/epics/bases/base-3.15.4
+EPICS_HOST_ARCH=centos7-x86_64
+EPICS_MODULES_PATH=/opt/epics/modules
+ENVIRONMENT_VERSION=2.0.0
+#EPICS_CA_ADDR_LIST=""
+TZ='Europe/Stockholm'
+# Specific configuration
+PROCSERV_PORT=2013
+EPICS_CAS_INTF_ADDR_LIST="192.168.10.115"
---
+++ ioc/nuc-01/ioc13-bake/st.cmd
@@ -0,0 +1,2 @@
+require ioc-bakeout-freia,konrad
+requireSnippet(bakeout-freia.cmd, "IP_ADDR=192.168.1.104:2020, DT320PREFIX=Ctrl-Bake")
