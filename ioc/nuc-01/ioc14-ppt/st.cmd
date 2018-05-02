50a21fcf44f499fda08edf819614567ea5a4d8bb
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2018-05-02 10:33:36 +0200

    Added ioc14 and ioc15 to nuc-01 (for PPT modulator and Toshiba klystron).

---
+++ ioc/nuc-01/ioc14-ppt/st.cmd
@@ -0,0 +1,2 @@
+require ioc-ppt-freia,konrad
+requireSnippet(ampegon-ppt-freia.cmd, "Px=HPRF-PPT,PORT_ADDR=192.168.5.4:8000")
---
+++ ioc/nuc-01/ioc15-rflps/st.cmd
@@ -0,0 +1,2 @@
+require ioc-rflps-ts1,konrad
+requireSnippet(rflps-toshiba.cmd)
