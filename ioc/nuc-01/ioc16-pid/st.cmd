660e57974f4ec86963c2cddca95a3da9c6a6cd25
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2018-08-24 14:26:39 +0200

    Added ioc for a simple pid.

---
+++ ioc/nuc-01/ioc16-pid/st.cmd
@@ -0,0 +1,13 @@
+#require autosave,5.7+
+require simple-pid-freia,konrad
+#epicsEnvSet("AUTOSAVE_SYSM_PV_PREFIX","simple-pid:")
+
+
+## Load record instances
+dbLoadRecords("simple-pid-freia.db", "P_MEAS='CstatH-LHe:PT102:sRdV',P_OUT='CstatH-MKS:MKS3:cOutput',SCAN='5 second'")
+
+#requireSnippet(rfleakdet-freia-preSaveRestore.cmd)
+
+iocInit
+
+#requireSnippet(rfleakdet-freia-postSaveRestore.cmd)
