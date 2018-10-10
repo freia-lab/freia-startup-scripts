c567ddeebd07bb8d5ac25938486fb587832f22d7
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2018-10-10 17:42:36 +0200

    Updated the scripts for gersemi and linde.

--- ioc/nuc-01/ioc11-gersemi/st.cmd
+++ ioc/nuc-01/ioc11-gersemi/st.cmd
@@ -1,3 +1,4 @@
-require ioc-gersemi-freia,konrad
-#require ioc-gersemi-freia,1.0+
-requireSnippet(gersemi-freia.cmd, "PLC_IPADDR=192.168.10.4")
+#require ioc-gersemi-freia,konrad
+require ioc-gersemi-freia,0.0+
+#requireSnippet(gersemi-freia.cmd, "PLC_IPADDR=192.168.10.4")
+requireSnippet(gersemi-freia.cmd, "PLC_IPADDR=192.168.1.30")
