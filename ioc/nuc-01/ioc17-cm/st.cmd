ba8fdc0d3ba14999b140e2dcef78a425a313470b
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2019-06-28 10:48:59 +0200

    Updated hnoss ioc scripts. Added cryomodule scripts.

---
+++ ioc/nuc-01/ioc17-cm/st.cmd
@@ -0,0 +1,13 @@
+#require autosave,5.7+
+#require ioc_cm_freia,konrad
+require ioc_cm_freia,0.20+
+
+requireSnippet(cm-freia.cmd, "PLC_IPADDR=192.168.1.11, KEITH_IPADDR=192.168.1.12:5025")
+
+dbpf CM-VBox:LT03:sRdV.LOLO 2
+dbpf CM-CM:LT01:sRdV.LOLO 2
+dbpf CM-VBox:LT03:sRdV.HYST 3
+dbpf CM-CM:LT01:sRdV.HYST 3
+dbpf CM-CM:LT01:sRdV.LLSV MAJOR
+dbpf CM-VBox:LT03:sRdV.LLSV MAJOR
+
