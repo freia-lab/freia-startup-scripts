2d72819d1cee72887bf3e50e110d89133f03181f
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2019-07-29 15:55:33 +0200

    Added writing messages to the syslog facility.

--- home.root/rotateLogs.sh
+++ home.root/rotateLogs.sh
@@ -3,14 +3,14 @@ h=`hostname`
 cd /var/run/procServ
 for f in *; do
     if [ -d ${f} ]; then
-	echo "logrotate /opt/startup/ioc/$h/$f/logrotate.conf"
+	logger -t rotateLogs "logrotate /opt/startup/ioc/$h/$f/logrotate.conf"
         pid=`systemctl status ioc@$f | awk '/Main PID:/ {print $3}'`
         f=/opt/startup/ioc/$h/$f/logrotate.conf
 	if [ -f "$f" ]; then
             logrotate $f
         fi
         if [ ! -z "$pid" ]; then
-            echo "kill -SIGHUP $pid"
+            logger -t rotateLogs "kill -SIGHUP $pid"
             kill -SIGHUP $pid
 	fi  
     fi
