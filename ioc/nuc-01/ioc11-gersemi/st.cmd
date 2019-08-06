0fd9d709b6a0fb24b654ca7f5347425152691e2c
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2019-08-06 14:21:56 +0200

    Added tagged version of gersemi ioc.

--- ioc/nuc-01/ioc11-gersemi/st.cmd
+++ ioc/nuc-01/ioc11-gersemi/st.cmd
@@ -1,4 +1,3 @@
 #require ioc-gersemi-freia,konrad
-require ioc-gersemi-freia,0.0+
-#requireSnippet(gersemi-freia.cmd, "PLC_IPADDR=192.168.10.4")
+require ioc-gersemi-freia,0.6+
 requireSnippet(gersemi-freia.cmd, "PLC_IPADDR=192.168.1.30")
