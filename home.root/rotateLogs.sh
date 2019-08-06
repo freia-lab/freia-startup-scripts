d6cfaad80481ff98fb933e6558252445bcb79393
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2019-08-06 13:48:39 +0200

    Updated all files related to log file rotation.

--- home.root/rotateLogs.sh
+++ home.root/rotateLogs.sh
@@ -4,12 +4,12 @@ cd /var/run/procServ
 for f in *; do
     if [ -d ${f} ]; then
 	logger -t rotateLogs "logrotate /opt/startup/ioc/$h/$f/logrotate.conf"
-        pid=`systemctl status ioc@$f | awk '/Main PID:/ {print $3}'`
+        pid=`systemctl show ioc@$f | awk -F = '/ExecMainPID/ {print $2}'`
         f=/opt/startup/ioc/$h/$f/logrotate.conf
 	if [ -f "$f" ]; then
-            logrotate $f
+            /usr/sbin/logrotate $f
         fi
-        if [ ! -z "$pid" ]; then
+        if [ "$pid" != "0" ]; then
             logger -t rotateLogs "kill -SIGHUP $pid"
             kill -SIGHUP $pid
 	fi  
