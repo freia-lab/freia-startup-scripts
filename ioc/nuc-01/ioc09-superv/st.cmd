fcffbb1898d7124951bc2e10c54e4c1d12c89ea6
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2019-11-19 09:36:48 +0100

    New version of ioc_superv_freia.

--- ioc/nuc-01/ioc09-superv/st.cmd
+++ ioc/nuc-01/ioc09-superv/st.cmd
@@ -1,3 +1,3 @@
 #require ioc_superv_freia,konrad
-require ioc_superv_freia,1.12+
+require ioc_superv_freia,1.13+
 requireSnippet(superv-freia.cmd, "SUPERV_FREIA_1=192.168.10.60,SUPERV_FREIA_2=192.168.10.62,SUPERV_FREIA_3=130.238.200.137,SUPERV_FREIA_4=192.168.10.107,SUPERV_FREIA_5=192.168.10.108,SUPERV_FREIA_6=130.238.4.140,SUPERV_FREIA_7=192.168.10.70")
