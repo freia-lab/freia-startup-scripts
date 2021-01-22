c727a2eeae653620acd183c200ee5158d99eb78c
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2021-01-22 09:40:03 +0100

    Updated freia-cagw1 firewall rulles and ca gateway access rules.

--- boot/nuc-02/gateway/start_gateway.sh
+++ boot/nuc-02/gateway/start_gateway.sh
@@ -18,7 +18,7 @@ start()
 	echo "Starting CA gateway in $gwhome"
 	[ ! -d $gwhome ] && mkdir $gwhome
 	chown -R $uid:$gid $gwhome
-	EPICS_CA_MAX_ARRAY_BYTES=1600000 gateway -home $gwhome -log gateway.log -prefix GW-$host -cip 192.168.10.255 -sip 130.238.200.140 -signore $host_ip -archive -no_cache -pvlist /opt/startup/boot/$host/gateway/GATEWAY.pvlist -access /opt/startup/boot/$host/gateway/GATEWAY.access -uid $uid -gid $gid -server
+	EPICS_CA_MAX_ARRAY_BYTES=20000000 gateway -home $gwhome -log gateway.log -prefix GW-$host -cip 192.168.10.255 -sip 130.238.200.140 -signore $host_ip -archive -no_cache -pvlist /opt/startup/boot/$host/gateway/GATEWAY.pvlist -access /opt/startup/boot/$host/gateway/GATEWAY.access -uid $uid -gid $gid -server
 }
 
 stop()
