0408c541bae8b2e4fe4c0348fe35c2b9be63aea8
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2021-08-19 11:42:05 +0200

    Added support for NESSA radiation monitoring system.

--- boot/nuc-03/add_net.sh
+++ boot/nuc-03/add_net.sh
@@ -6,8 +6,12 @@ nmcli device show >/tmp/network_devices
 hostname >/tmp/hostname
 echo "Adding PLC1 connection"
 nmcli connection add type ethernet con-name PLC1 ifname enp0s20u3 ip4 192.168.1.117/24
+echo "Adding NESSA connection"
+nmcli connection add type ethernet con-name NESSA ifname enp0s20u4 ip4 192.168.21.117/24
 echo "Setting connection PLC1 up"
 nmcli connection up PLC1
+echo "Setting connection NESSA up"
+nmcli connection up NESSA
 
 echo "Setting up symmetric routing"
 ip route add 192.168.10.0/24 dev eno1 tab 1
@@ -15,6 +19,9 @@ ip route add 192.168.1.0/24 dev enp0s20u3 tab 2
 ip route add default via 192.168.10.1 dev eno1 tab 1
 ip rule add from 192.168.10.117/32 tab 1 priority 100
 ip rule add from 192.168.1.117/32 tab 2 priority 200
+
+ip route add 192.168.20.0/24 via 192.168.21.1 dev enp0s20u4
+
 ip route flush cache
 
 #ip route add default via 192.168.10.1 dev eno1
---
+++ e3ioc/nuc-03/ioc02-dpu3/st.cmd
@@ -0,0 +1,26 @@
+#require stream,2.8.10
+require dpu3,1.0.0
+ 
+#Specify the TCP endpoint and give your 'bus' an arbitrary name eg. "asynstream1".
+drvAsynIPPortConfigure("PORT1","192.168.20.102:5000",0,0,0)
+
+#drvAsynSerialPortConfigure ("asynport2","/dev/ttyS0")
+#asynSetOption ("asynport2", 0, "baud", "9600")
+#asynSetOption ("asynport2", 0, "bits", "8")
+#asynSetOption ("asynport2", 0, "parity", "none")
+#asynSetOption ("asynport2", 0, "stop", "2")
+#asynSetOption ("asynport2", 0, "clocal", "N")
+#asynSetOption ("asynport2", 0, "crtscts", "N")
+
+#Indicate the folder where the MB100A protocol file is installed
+epicsEnvSet("STREAM_PROTOCOL_PATH","$(dpu3_DB)")
+
+#Load your database defining the EPICS records
+dbLoadRecords("dpu3.db", "PORT=PORT1,P=RadProt-Bunker2:")
+
+asynSetTraceIOMask "PORT1",0,4   # Enable traceIOHex
+#asynSetTraceMask "PORT1",0,0x1f     # Enable traceError and traceIODriver
+#asynSetTraceMask "PORT1",0,0xfff     # Enable traceError and traceIODriver
+
+iocInit()
+
