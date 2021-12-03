dce46bb1541cbf184920a841bf6ab117aec956ba
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2021-12-03 14:21:59 +0100

    Updated st.cmd for the new version of e3-dcbreaker (1.1.0).

--- e3ioc/nuc-03/ioc04-dcbreaker/st.cmd
+++ e3ioc/nuc-03/ioc04-dcbreaker/st.cmd
@@ -1,43 +1,12 @@
-require dcbreaker,1.0.0
+require dcbreaker,1.1.0
  
-#Specify the TCP endpoint and give your 'bus' an arbitrary name eg. "asynstream1".
-#drvAsynIPPortConfigure 'port name' 'host:port [protocol]' priority 'disable auto-connect' noProcessEos
+require recsync
 
-drvAsynIPPortConfigure("PORT1","192.168.10.42:502",0,0,1)
-drvAsynIPPortConfigure("PORT2","192.168.10.43:502",0,0,1)
+epicsEnvSet ("IOCNAME", "ioc04-dcbreaker")
 
-#drvAsynIPPortConfigure("PORT1","192.168.1.103:502",0,0,1)
+iocshLoad("$(dcbreaker_DIR)/dcbreaker.iocsh", "ASYN_PORT_NAME=PORT1,DCBREAKER_IP=192.168.10.42,PREFIX=MAG-,DEV_NAME=EE-1:")
+iocshLoad("$(dcbreaker_DIR)/dcbreaker.iocsh", "ASYN_PORT_NAME=PORT2,DCBREAKER_IP=192.168.10.43,PREFIX=MAG-,DEV_NAME=EE-2:")
 
-#modbusInterposeConfig(portName, 
-#                      linkType,
-#                      timeoutMsec,
-#                      writeDelayMsec)
-
-modbusInterposeConfig("PORT1",0,2000,0)
-modbusInterposeConfig("PORT2",0,2000,0)
-
-#drvModbusAsynConfigure	 'Port name'
-#			 'Octet port name' 
-#			 'Modbus slave address' 
-#			 'Modbus function code' 
-#			 'Modbus start address' 
-#			 'Modbus length' 
-#			 'Data type' 
-#			 'Poll time (msec)' 
-#			 'PLC type'
-
-drvModbusAsynConfigure(PORT1_DATA, "PORT1", 1, 4, 0, 85, "INT16", 2000, "astrol")
-drvModbusAsynConfigure(PORT1_DATA_PARAM, "PORT1", 1, 3, 16, 4, "INT16", 2000, "astrol")
-drvModbusAsynConfigure(PORT2_DATA, "PORT2", 1, 4, 0, 85, "INT16", 2000, "astrol")
-drvModbusAsynConfigure(PORT2_DATA_PARAM, "PORT2", 1, 3, 16, 4, "INT16", 2000, "astrol")
-
-#Load your database defining the EPICS records
-dbLoadRecords("dcbreaker.db", "PORT_DATA=PORT1_DATA,PORT_PARAM=PORT1_DATA_PARAM,P=MAG-,D=EE-1:")
-dbLoadRecords("dcbreaker.db", "PORT_DATA=PORT2_DATA,PORT_PARAM=PORT2_DATA_PARAM,P=MAG-,D=EE-2:")
-
-
-#asynSetTraceIOMask "PORT1",0,4   # Enable traceIOHex
-#asynSetTraceMask "PORT1",0,0x1f     # Enable traceError and traceIODriver
-
-iocInit()
+dbLoadRecords("asynRecord.db","P=$(IOCNAME),R=:asynRec,PORT='PORT1',ADDR='0',IMAX='1024',OMAX='256'")
+iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=$(IOCNAME)")
 
