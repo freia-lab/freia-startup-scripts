284e7da845767ab545087f3e4706a4b220e5b68b
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2021-12-15 11:34:33 +0100

    Moved ioc08-misc IOC from EEE to E3 environment.

---
+++ e3ioc/nuc-01/ioc08-misc/st.cmd
@@ -0,0 +1,8 @@
+require miscfreia
+require recsync
+
+epicsEnvSet ("IOCNAME", "ioc08-misc")
+
+iocshLoad("$(miscfreia_DIR)/miscfreia.iocsh",  "TOP=/opt/epics/autosave,IP_ADDR=192.168.1.103,IP_ADDR2=192.168.1.59:1137,IP_ADDR3=192.168.1.58:1137,IP_ADDR4=192.168.10.188:1137")
+iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=$(IOCNAME)")
+
--- ioc/nuc-01/ioc08-misc/st.cmd
+++
@@ -1,5 +0,0 @@
-require asyn,4.31.0+
-require ioc_misc_freia,1.20+
-#require ioc_misc_freia,konrad
-requireSnippet(misc-freia.cmd)
-
