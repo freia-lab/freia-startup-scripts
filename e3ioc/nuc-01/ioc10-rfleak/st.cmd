5ae8ad7effaa30a79043ff34bf8c9f5b429dee51
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2021-12-23 12:05:22 +0100

    Moved ioc10-rfleak from EEE to E3 environment (start-up scrpits from ioc to e3ioc directory).

---
+++ e3ioc/nuc-01/ioc10-rfleak/st.cmd
@@ -0,0 +1,22 @@
+epicsEnvSet ("IOCNAME", "ioc10-rfleak")
+
+# Directory name for the autosave files
+epicsEnvSet ("IOCDIR", "rfleakdet")
+
+require rfleakdet
+require autosave
+require recsync
+
+iocshLoad("$(rfleakdet_DIR)/rfleakdet.iocsh", "ASYN_PORT_NAME=RFLEAKDET")
+
+dbLoadRecords("asynRecord.db","P='$(IOCNAME)',R=:asynRec,PORT='${RFLEAK_PORT}',ADDR='0',IMAX='1024',OMAX='256'")
+## Load record instances
+dbLoadRecords("rfleakdet-main.db","RFDET_PORT='${RFLEAK_PORT}',PREFIX='Env-RFleakage'")
+dbLoadRecords("rfleakdet.db","RFDET_PORT='${RFLEAK_PORT}',PREFIX='Env-RFleakage',NODE=0")
+dbLoadRecords("rfleakdet.db","RFDET_PORT='${RFLEAK_PORT}',PREFIX='Env-RFleakage',NODE=1")
+dbLoadRecords("rfleakdet.db","RFDET_PORT='${RFLEAK_PORT}',PREFIX='Env-RFleakage',NODE=2")
+dbLoadRecords("rfleakdet.db","RFDET_PORT='${RFLEAK_PORT}',PREFIX='Env-RFleakage',NODE=3")
+
+iocshLoad("$(autosave_DIR)/autosave.iocsh", "AS_TOP=/opt/epics/autosave,IOCNAME=$(IOCNAME)")
+iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=$(IOCNAME)")
+
