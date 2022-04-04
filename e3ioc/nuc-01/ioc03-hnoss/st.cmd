7f9b843063a44e05a3dbb8ca499116263322bab8
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2022-04-04 17:30:33 +0200

    Moved ioc03-hnoss to E3 environment.

---
+++ e3ioc/nuc-01/ioc03-hnoss/st.cmd
@@ -0,0 +1,14 @@
+require hnoss
+require recsync
+require iocstats
+
+epicsEnvSet("IOCNAME", "ioc03-hnoss")
+
+iocshLoad("$(hnoss_DIR)/hnoss.iocsh", "PLC_IP=192.168.1.20, TOP=/opt/epics/autosave, IOCNAME=$(IOCNAME)")
+
+# iocStats database
+dbLoadRecords("$(iocstats_DB)/iocAdminSoft-ess.db","IOC=$(IOCNAME)")
+
+# Start recsync client
+iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=$(IOCNAME)")
+
--- ioc/nuc-01/ioc03-hnoss/st.cmd
+++
@@ -1,3 +0,0 @@
-#require ioc-hnoss-freia,konrad
-require ioc-hnoss-freia,1.24+
-requireSnippet(hnoss-freia.cmd, "PLC_IPADDR=192.168.1.20")
