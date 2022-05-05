3ac2761495e88af48801ad01db14bd41de79fa86
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2022-05-05 20:31:07 +0200

    Ported ioc11-gersemi to e3 environment.

---
+++ e3ioc/nuc-01/ioc11-gersemi/st.cmd
@@ -0,0 +1,14 @@
+require gersemi
+require recsync
+require iocstats
+
+epicsEnvSet("IOCNAME", "ioc11-gersemi")
+
+iocshLoad("$(gersemi_DIR)/gersemi.iocsh", "PLC_IP=192.168.1.30, TOP=/opt/epics/autosave, IOCNAME=$(IOCNAME)")
+
+# iocStats database
+dbLoadRecords("$(iocstats_DB)/iocAdminSoft-ess.db","IOC=$(IOCNAME)")
+
+# Start recsync client
+iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=$(IOCNAME)")
+
--- ioc/nuc-01/ioc11-gersemi/env.sh
+++
@@ -1,9 +0,0 @@
-# General configuration
-BASE=3.15.4
-EPICS_BASE=/opt/epics/bases/base-3.15.4
-EPICS_HOST_ARCH=centos7-x86_64
-EPICS_MODULES_PATH=/opt/epics/modules
-ENVIRONMENT_VERSION=2.0.0
-# Specific configuration
-PROCSERV_PORT=2011
-EPICS_CAS_INTF_ADDR_LIST="192.168.10.115"
