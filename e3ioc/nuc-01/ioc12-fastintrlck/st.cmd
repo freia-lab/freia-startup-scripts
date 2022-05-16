36f613d8a74eb8d22084785af733ffd33b1fc3c6
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2022-05-16 19:42:18 +0200

    Ported ioc12-seq to E3 environment (ioc12-fastintrlck).

---
+++ e3ioc/nuc-01/ioc12-fastintrlck/st.cmd
@@ -0,0 +1,14 @@
+require fastintrlck
+require recsync
+require iocstats
+
+epicsEnvSet("IOCNAME", "ioc12-fastintrlck")
+
+iocshLoad("$(fastintrlck_DIR)/fastintrlck.iocsh", "P=IntlkF-cRio1, TOP=/opt/epics/autosave, IOCNAME=$(IOCNAME)")
+
+# iocStats database
+dbLoadRecords("$(iocstats_DB)/iocAdminSoft-ess.db","IOC=$(IOCNAME)")
+
+# Start recsync client
+iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=$(IOCNAME)")
+
--- ioc/nuc-01/ioc12-seq/st.cmd
+++
@@ -1,4 +0,0 @@
-require ioc_seq_freia,1.3+
-#require ioc_seq_freia,konrad
-requireSnippet(seq-freia.cmd, "P=IntlkF-cRio1")
-
