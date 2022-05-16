9ebd009117be0a8a3cd4724ac2a118bc3f51583f
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2022-05-16 13:46:30 +0200

    Moved ioc17-cm to E3 environment.

---
+++ e3ioc/nuc-01/ioc17-cm/st.cmd
@@ -0,0 +1,20 @@
+require cm
+require recsync
+require iocstats
+
+epicsEnvSet("IOCNAME", "ioc17-cm")
+
+iocshLoad("$(cm_DIR)/cm.iocsh", "PLC_IP=192.168.1.11, TOP=/opt/epics/autosave, IOCNAME=$(IOCNAME)")
+
+# iocStats database
+dbLoadRecords("$(iocstats_DB)/iocAdminSoft-ess.db","IOC=$(IOCNAME)")
+
+# Start recsync client
+iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=$(IOCNAME)")
+
+
+#dbpf CM-VBox:LT03:sRdV.LOLO 2
+#dbpf CM-CM:LT01:sRdV.LOLO 2
+#dbpf CM-VBox:LT03:sRdV.HYST 3
+#dbpf CM-CM:LT01:sRdV.HYST 3
+
