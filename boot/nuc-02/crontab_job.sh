1fdae39bb6e5aa65c96768bb495f4c4853f573d4
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2022-06-13 15:00:41 +0200

    Updated ca gateway pv list. Added IOC_NAME definition for tpg300.

--- boot/nuc-02/crontab_job.sh
+++ boot/nuc-02/crontab_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 hostn=`hostname`
-echo "Adding crontab job for monitoring /var partition usage"
+echo "Adding crontab job for logrotate and monitoring /var partition usage"
 cp /opt/startup/boot/$hostn/crontab.root /var/spool/cron/root
 systemctl reload crond
