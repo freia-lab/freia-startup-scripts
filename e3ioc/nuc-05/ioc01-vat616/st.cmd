3e997b851de27ee136b6b50b6edb8a2421d2cd09
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2022-09-03 18:33:28 +0200

    Added start-up script for pnan5221 on nuc-05.

--- e3ioc/nuc-05/ioc01-vat616/st.cmd
+++ e3ioc/nuc-05/ioc01-vat616/st.cmd
@@ -4,7 +4,7 @@ require iocstats
 
 epicsEnvSet ("IOCNAME", "ioc01-vat616")
 
-iocshLoad(${vat616_DIR}/vat616_server.iocsh, "IOCNAME = ioc-vat616, IP_ADDR = 192.168.10.110")
+iocshLoad(${vat616_DIR}/vat616_server.iocsh, "IOCNAME = ioc01-vat616, IP_ADDR = 192.168.10.110")
 
 iocshLoad(${vat616_DIR}/vat616_valve.iocsh, "PREFIX=CstatV-RHtr, V=CV580, INDX=0")
 iocshLoad(${vat616_DIR}/vat616_valve.iocsh, "PREFIX=CstatV-RHtr, V=CV581, INDX=1")
---
+++ e3ioc/nuc-05/ioc02-pnan5221/st.cmd
@@ -0,0 +1,22 @@
+require pnan5221
+require recsync
+require iocstats
+
+epicsEnvSet ("IOCNAME", "ioc02-pnan5221")
+
+# One can add the database used by the scan server scripts or it can be used in a dedicated IOC
+#
+# This is for scanning motor 1 (resonance frequency read from trace 2 of the spectrum vector analuzer)
+#dbLoadRecords("$(pnan5221_DB)mtr-scan.template", "P=LLRF:,N=1,MRKR=2")
+# This is for scanning motor 2 (resonance frequency read from trace 3 of the spectrum vector analuzer)
+#dbLoadRecords("$(pnan5221_DB)mtr-scan.template", "P=LLRF:,N=2,MRKR=3")
+
+iocshLoad(${pnan5221_DIR}/pnan5221.iocsh, "IOCNAME = ioc02-pnan5221, IP_ADDR = 192.168.10.33")
+
+# iocStats database
+
+dbLoadRecords("$(iocstats_DB)/iocAdminSoft-ess.db","IOC=$(IOCNAME)")
+
+# Start recsync client
+
+iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=$(IOCNAME)")
