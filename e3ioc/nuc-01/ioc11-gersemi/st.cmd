8bc5a4249ee1816aecd59fc913d165bcdac7a40e
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2023-01-25 14:51:39 +0100

    Increased the callback queue buffer for Gersemi IOC.

--- e3ioc/nuc-01/ioc11-gersemi/st.cmd
+++ e3ioc/nuc-01/ioc11-gersemi/st.cmd
@@ -4,6 +4,9 @@ require iocstats
 
 epicsEnvSet("IOCNAME", "ioc11-gersemi")
 
+callbackSetQueueSize(5000)
+dbPvdTableSize(1024)
+
 iocshLoad("$(gersemi_DIR)/gersemi.iocsh", "PLC_IP=192.168.1.30, TOP=/opt/epics/autosave, IOCNAME=$(IOCNAME)")
 
 # iocStats database
