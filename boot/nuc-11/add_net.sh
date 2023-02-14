4605a4eba67397bc22b36408891716fdc1e9d2e0
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2023-02-14 17:03:56 +0100

    Added startup scripts for the new NUCs.

---
+++ boot/nuc-11/add_net.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+host=`hostname`
+echo "Stopping LDAP client daemon"
+systemctl stop nslcd
+nmcli device show >/tmp/network_devices
+hostname >/tmp/hostname
+#echo "Adding LLAB connection"
+#nmcli connection add type ethernet con-name LLAB ifname enp0s21f0u3 ip4 10.1.11.31/16 
+#echo "Setting connection LLAB up"
+#nmcli connection up LLAB
+
+#echo "Setting up symmetric routing"
+#ip route add 192.168.10.0/24 dev eno1 tab 1
+#ip route add 10.1.0.0/16 dev enp0s21f0u3 tab 2
+#ip route add default via 192.168.10.1 dev eno1 tab 1
+#ip rule add from 192.168.10.128/32 tab 1 priority 100
+#ip rule add from 10.1.11.31/32 tab 2 priority 200
+
+#ip route flush cache
+
+#ip route add default via 192.168.10.1 dev eno1
---
+++ e3ioc/nuc-08/ioc20-laser/st.cmd
@@ -0,0 +1,19 @@
+require laser
+require recsync
+require iocstats
+
+#iocshLoad("$(laser_DIR)/laser.iocsh", "IOCNAME=ioc20-laser")
+iocshLoad("$(laser_DIR)/laser.iocsh", "IOCNAME=ioc20-laser, LASER_IP=http://10.1.251.2:20010, OPA_IP=http://10.1.11.11:8004, EPP_IP=http://10.1.11.11:14001, P=Llab-, SCAN='2 second'")
+
+epicsEnvShow PYTHONPATH
+
+pydev("laser.debug=0")
+
+pydev("opa.debug=0")
+
+# iocStats database
+dbLoadRecords("$(iocstats_DB)/iocAdminSoft-ess.db","IOC=ioc20-laser")
+
+# Start recsync client
+iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=ioc20-laser")
+
