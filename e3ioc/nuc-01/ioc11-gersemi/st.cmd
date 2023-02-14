3d7ef66f42d11acfc743c847d159df9123f34e73
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2023-02-14 11:22:35 +0100

    Increased callback queue size. Set up alarms for it.

--- e3ioc/nuc-01/ioc11-gersemi/st.cmd
+++ e3ioc/nuc-01/ioc11-gersemi/st.cmd
@@ -4,7 +4,7 @@ require iocstats
 
 epicsEnvSet("IOCNAME", "ioc11-gersemi")
 
-callbackSetQueueSize(5000)
+callbackSetQueueSize(30000)
 dbPvdTableSize(1024)
 
 iocshLoad("$(gersemi_DIR)/gersemi.iocsh", "PLC_IP=192.168.1.30, TOP=/opt/epics/autosave, IOCNAME=$(IOCNAME)")
@@ -12,6 +12,13 @@ iocshLoad("$(gersemi_DIR)/gersemi.iocsh", "PLC_IP=192.168.1.30, TOP=/opt/epics/a
 # iocStats database
 dbLoadRecords("$(iocstats_DB)/iocAdminSoft-ess.db","IOC=$(IOCNAME)")
 
+dbLoadRecords("asynRecord.db","P=${IOCNAME},R=:asynRec,PORT=G_S7,ADDR='0',IMAX='1024',OMAX='256'")
+
 # Start recsync client
 iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=$(IOCNAME)")
+
+afterInit(dbpf ioc11-gersemi:CBLOW_Q_HIGH.HIGH 600)
+afterInit(dbpf ioc11-gersemi:CBLOW_Q_HIGH.HIHI 1000)
+afterInit(dbpf ioc11-gersemi:CBLOW_Q_HIGH.HSV "MINOR")
+afterInit(dbpf ioc11-gersemi:CBLOW_Q_HIGH.HHSV "MAJOR")
 
