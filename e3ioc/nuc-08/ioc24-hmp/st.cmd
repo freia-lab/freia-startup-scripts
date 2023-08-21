3edf5c96f4544846ce617634c2201ca9fbd8c6f3
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2023-08-21 09:44:47 +0200

    Added ioc24-hmp on nuc-08.

---
+++ e3ioc/nuc-08/ioc24-hmp/st.cmd
@@ -0,0 +1,17 @@
+require hmp40x0
+require recsync
+require iocstats
+
+# Limit the error messages form the StreamDevice (specially when the device is not conn
+ected)
+var streamErrorDeadTime 60
+
+iocshLoad("$(hmp40x0_DIR)hmp4040.iocsh", "IP=192.168.10.45,P=CstatV-AC:,IOCNAME=ioc24-hmp")
+
+
+# iocStats database
+#dbLoadRecords("$(iocstats_DB)/iocAdminSoft-freia.db","IOC=ioc24-hmp")
+
+# Start recsync client
+#iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=ioc24-hmp")
+
