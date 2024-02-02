d4241129a145363f842b35f60793fdd6eaa0c16a
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2024-02-02 09:31:38 +0100

    Added a nuc-09 booting Ubuntu OS. Installed ioc27-htrctrl on it.

--- boot/nuc-09/add_net.sh
+++ boot/nuc-09/add_net.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
-device=enx00249b6dcb8b
 host=`hostname`
 ip link  >/tmp/network_devices
 hostname >/tmp/hostname
-#ip link add $device type ethernet
-#Set address for dev $device
-echo "Setting address for $device"
-ip addr add 130.238.200.145/25 dev $device
-#ip link set dev $device up
-ip link set dev $device up
-echo "Setting up symmetric routing"
-ip route add 192.168.10.0/24 dev eno1 tab 1
-ip route add 130.238.200.128/25 dev $device tab 2
-ip route add default via 130.238.200.129 dev $device tab 2
-ip route add default via 192.168.10.1 dev eno1 tab 1
-ip rule add from 192.168.10.129/32 tab 1 priority 100
-ip rule add from 130.238.200.145/32 tab 2 priority 200
-ip route flush cache
-
 touch /tmp/nuc-09
-echo "Setting up firewall rules"
-iptables-restore </opt/startup/boot/$host/iptables.rules
---
+++ e3ioc/nuc-09/ioc27-htrctrl/st.cmd
@@ -0,0 +1,26 @@
+require heaterctrl
+
+require recsync
+require iocstats
+
+addScan(60)
+addScan(30)
+
+epicsEnvSet("IOCNAME", "ioc27-htrctrl")
+epicsEnvSet("IP", "192.168.10.44")
+epicsEnvSet("P", "CstatV-AC:")
+epicsEnvSet("TOP", "/opt/epics/autosave")
+
+iocshLoad("$(daq6510_DIR)daq6510_init.iocsh", "ASYN_PORT=DAQ_PORT,IP=192.168.10.44,P=CstatV-AC:")
+
+dbLoadRecords("asynRecord.db","P=${IOCNAME},R=:asynRec,PORT=$(ASYN_PORT=DAQ_PORT),ADDR='0',IMAX='1024',OMAX='256'")
+dbLoadRecords("heaterctrl.db", "P=CstatV-AC:, DEV=DAQ6510, PORT=DAQ_PORT")
+
+iocshLoad("$(autosave_DIR)/autosave.iocsh", "AS_TOP=$(TOP),IOCNAME=$(IOCNAME),IOCDIR=$(IOCDIR=daq6510)")
+
+# iocStats database
+dbLoadRecords("$(iocstats_DB)/iocAdminSoft-freia.db","IOC=ioc27-htrctrl")
+
+# Start recsync client
+#iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=ioc27-htrctrl")
+
