918d60a908a2e36d8a0c832783cfae4c89a9c2db
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2024-02-05 15:19:30 +0100

    Added start-up scripts for rexroth IOC.

---
+++ e3ioc/nuc-09/ioc26-rexroth/st.cmd
@@ -0,0 +1,22 @@
+require rexroth
+
+require recsync
+require iocstats
+
+
+epicsEnvSet("IOCNAME", "ioc26-rexroth")
+epicsEnvSet("TOP", "/opt/epics/autosave")
+
+# Limit the error messages form the StreamDevice (specially when the device is not connected)
+var streamErrorDeadTime 60
+
+iocshLoad("$(rexroth_DIR)rexroth.iocsh", "IP=192.168.10.108,P=CstatV-AC:,IP_PORT=2195,IOCNAME=ioc26-rexroth")
+
+#iocshLoad("$(autosave_DIR)/autosave.iocsh", "AS_TOP=$(TOP),IOCNAME=$(IOCNAME),IOCDIR=$(IOCDIR=rexroth)")
+
+# iocStats database
+dbLoadRecords("$(iocstats_DB)/iocAdminSoft-freia.db","IOC=${IOCNAME}")
+
+# Load the recsync client's database
+iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=${IOCNAME}")
+
