33ccc51268eec5efa517646d303ef4c1b6be3e92
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2024-08-08 14:17:07 +0200

    Migrated IOCs on nuc-08 to Ubuntu.

--- boot/nuc-08/add_net.sh
+++ boot/nuc-08/add_net.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
+device=enx0c0e768ff587
 host=`hostname`
-echo "Stopping LDAP client daemon"
-systemctl stop nslcd
-nmcli device show >/tmp/network_devices
+ip link  >/tmp/network_devices
 hostname >/tmp/hostname
-echo "Adding LLAB connection"
-nmcli connection add type ethernet con-name LLAB ifname enp0s21f0u3 ip4 10.1.11.31/16 
-echo "Setting connection LLAB up"
-nmcli connection up LLAB
-
+echo "Setting address for $device"
+ip addr add 10.1.11.31/16 dev $device
+#ip link set dev $device up
+ip link set dev $device up
 echo "Setting up symmetric routing"
 ip route add 192.168.10.0/24 dev eno1 tab 1
-ip route add 10.1.0.0/16 dev enp0s21f0u3 tab 2
+ip route add 10.1.0.0/16 dev $device tab 2
+# ip route add default via xxx dev $device tab 2
 ip route add default via 192.168.10.1 dev eno1 tab 1
-ip rule add from 192.168.10.128/32 tab 1 priority 100
+ip rule add from 192.168.10.129/32 tab 1 priority 100
 ip rule add from 10.1.11.31/32 tab 2 priority 200
-
 ip route flush cache
 
-#ip route add default via 192.168.10.1 dev eno1
+echo "Stopping LDAP client daemon"
+systemctl stop nslcd
+# nmcli device show >/tmp/network_devices
+# echo "Adding LLAB connection"
+# nmcli connection add type ethernet con-name LLAB ifname enp0s21f0u3 ip4 10.1.11.31/16 
+# echo "Setting connection LLAB up"
+# nmcli connection up LLAB
+
+# echo "Setting up symmetric routing"
+# ip route add 192.168.10.0/24 dev eno1 tab 1
+# ip route add 10.1.0.0/16 dev enp0s21f0u3 tab 2
+# ip route add default via 192.168.10.1 dev eno1 tab 1
+# ip rule add from 192.168.10.128/32 tab 1 priority 100
+# ip rule add from 10.1.11.31/32 tab 2 priority 200
+
+# ip route flush cache
+
--- e3ioc/nuc-08/ioc18-hevol/st.cmd
+++ e3ioc/nuc-08/ioc18-hevol/st.cmd
@@ -6,7 +6,7 @@ epicsEnvSet ("IOCNAME", "ioc18-hevol")
 
 # iocStats database
 
-dbLoadRecords("$(iocstats_DB)/iocAdminSoft-ess.db","IOC=$(IOCNAME)")
+dbLoadRecords("$(iocstats_DB)/iocAdminSoft-freia.db","IOC=$(IOCNAME)")
  
 iocshLoad("$(hevol_DIR)/hevol.iocsh", "TOP=/opt/epics/autosave")
 iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=$(IOCNAME)")
