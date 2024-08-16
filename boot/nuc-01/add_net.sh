a56c0480c6d26c1064987f49c5f7918d1fa56e92
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2024-08-16 09:46:47 +0200

    Modified boot/nuc-01/add_net.sh.

--- boot/nuc-01/add_net.sh
+++ boot/nuc-01/add_net.sh
@@ -3,10 +3,17 @@
 device1=enx000ec6f92147
 device2=enx000ec6fa0b01
 nmcli device show >/tmp/network_devices
-nmcli connection add type ethernet con-name PLC1 ifname $device1 ip4 192.168.1.115/24
-nmcli connection add type ethernet con-name ESYS ifname $device2 ip4 192.168.4.115/24
-nmcli connection up PLC1
-nmcli connection up ESYS
+#nmcli connection add type ethernet con-name PLC1 ifname $device1 ip4 192.168.1.115/24
+#nmcli connection add type ethernet con-name ESYS ifname $device2 ip4 192.168.4.115/24
+
+#nmcli connection up PLC1
+ip addr add 192.168.1.115/24 dev $device1
+ip link set dev $device1 up
+
+#nmcli connection up ESYS
+ip addr add 192.168.4.115/24 dev $device2
+ip link set dev $device2 up
+
 echo "Setting up symmetric routing"
 ip route add 192.168.10.0/24 dev eno1 tab 1
 ip route add 192.168.1.0/24 dev $device1 tab 2
