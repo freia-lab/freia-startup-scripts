5f54aeb06bbcb506d24c42f2f2504c14af5d5cc8
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2024-09-07 17:01:19 +0200

    Fixed file permissions for crontab jobs.

--- boot/nuc-05/crontab_job.sh
+++ boot/nuc-05/crontab_job.sh
@@ -2,4 +2,5 @@
 hostn=`hostname`
 echo "Adding crontab job for monitoring /var partition usage and logrotate"
 cp /opt/startup/boot/$hostn/crontab.root /var/spool/cron/crontabs/root
+chmod 0600 /var/spool/cron/crontabs/root
 systemctl restart cron
