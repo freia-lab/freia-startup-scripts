a837c97685250ecc86058cc4bab50511f5cb2047
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2024-10-09 16:38:10 +0200

    Added cavity selection management stuff.

--- e3ioc/nuc-08/ioc28-arduinoplc/st.cmd
+++ e3ioc/nuc-08/ioc28-arduinoplc/st.cmd
@@ -9,5 +9,6 @@ epicsEnvSet ("IOCNAME", "ioc28-arduinoplc")
 dbLoadRecords("$(iocstats_DB)/iocAdminSoft-freia.db","IOC=$(IOCNAME)")
  
 iocshLoad("$(arduinoplc_DIR)/arduinoplc.iocsh", "ASYN_PORT=PORT1,IP_ADDR=192.168.10.40,TOP=/opt/epics/autosave,IOCNAME=ioc28-arduinoplc,PREFIX=LLRF-Aux:,DEV_NAME=ArduinoPMC:")
+iocshLoad("$(arduinoplc_DIR)/cav-selector.iocsh")
 iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=$(IOCNAME)")
 
