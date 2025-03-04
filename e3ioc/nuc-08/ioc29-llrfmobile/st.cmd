0c84914e27b0c6ff609b8138271752d2b7984848
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2025-03-04 14:35:14 +0100

    A new start-up script for ioc29-mobilellrf (for system with S7-1200 PLC).

--- e3ioc/nuc-08/ioc29-llrfmobile/st.cmd
+++ e3ioc/nuc-08/ioc29-llrfmobile/st.cmd
@@ -1,4 +1,7 @@
 require mobilellrf
+# If using the old version with Arduino PLC
+#require mobilellrf,1.4.0+2
+
 require recsync
 require iocstats
 
@@ -10,7 +13,11 @@ epicsEnvSet("IOCDIR", "llrfmobile")
 
 dbLoadRecords("$(iocstats_DB)iocAdminSoft-freia.db","IOC=$(IOCNAME)")
 
-iocshLoad("$(mobilellrf_DIR)mobilellrf.iocsh", "IP_ADDR1=192.168.10.109,PREFIX1=LLRF-1:,DEVICE1=FUNCGEN-1,ASYN_PORT1=PORT1,IP_ADDR2=192.168.10.40,PREFIX2=LLRF-Aux:,DEVICE2=ArduinoPMC:,ASYN_PORT2=PORT2,PREFIX3=LLRF-Ctrl:,IP_ADDR3=192.168.10.31,ASYN_PORT3=PORT3,IP_ADDR4=192.168.10.35,ASYN_PORT4=PORT4")
+# Start-up script for Arduino PMC version (with corresponding module mobilellrf version 1.4.0+2)
+#iocshLoad("$(mobilellrf_DIR)mobilellrf.iocsh", "IP_ADDR1=192.168.10.109,PREFIX1=LLRF-1:,DEVICE1=FUNCGEN-1,ASYN_PORT1=PORT1,IP_ADDR2=192.168.10.40,PREFIX2=LLRF-Aux:,DEVICE2=ArduinoPMC:,ASYN_PORT2=PORT2,PREFIX3=LLRF-Ctrl:,IP_ADDR3=192.168.10.31,ASYN_PORT3=PORT3,IP_ADDR4=192.168.10.35,ASYN_PORT4=PORT4")
+
+# Start-up script for the version with SIemens S7-1200 PLC
+iocshLoad("$(mobilellrf_DIR)mobilellrf-s7.iocsh", "IP_ADDR1=192.168.10.109,PREFIX1=LLRF-1:,DEVICE1=FUNCGEN-1,ASYN_PORT1=PORT1,IP_ADDR2=192.168.10.47,PREFIX2=LLRF-Ctrl:,ARDUINO_PREFIX=LLRF-Aux:,DEVICE2=ArduinoPMC:,ASYN_PORT2=PLC_PORT,PREFIX3=LLRF-Ctrl:,IP_ADDR3=192.168.10.31,ASYN_PORT3=PORT3,IP_ADDR4=192.168.10.35,ASYN_PORT4=PORT4")
 
 iocshLoad("$(recsync_DIR)recsync.iocsh", "IOCNAME=$(IOCNAME)")
 
