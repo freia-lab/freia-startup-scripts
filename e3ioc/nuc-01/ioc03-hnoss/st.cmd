11caed2541b1912ce7fc0c2b7e8767fde4064415
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2025-05-05 11:59:09 +0200

    Inceased calback queue size and added the alarms to $(IOCNAME):CBLOW_Q_HIGH.

--- e3ioc/nuc-01/ioc03-hnoss/st.cmd
+++ e3ioc/nuc-01/ioc03-hnoss/st.cmd
@@ -2,6 +2,8 @@ require hnoss
 require recsync
 require iocstats
 
+callbackSetQueueSize(30000)
+
 epicsEnvSet("IOCNAME", "ioc03-hnoss")
 
 iocshLoad("$(hnoss_DIR)/hnoss.iocsh", "PLC_IP=192.168.1.20, TOP=/opt/epics/autosave, IOCNAME=$(IOCNAME)")
@@ -11,4 +13,9 @@ dbLoadRecords("$(iocstats_DB)/iocAdminSoft-freia.db","IOC=$(IOCNAME)")
 
 # Start recsync client
 iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=$(IOCNAME)")
+
+afterInit(dbpf $(IOCNAME):CBLOW_Q_HIGH.HIGH 600)
+afterInit(dbpf $(IOCNAME):CBLOW_Q_HIGH.HIHI 1000)
+afterInit(dbpf $(IOCNAME):CBLOW_Q_HIGH.HSV "MINOR")
+afterInit(dbpf $(IOCNAME):CBLOW_Q_HIGH.HHSV "MAJOR")
 
