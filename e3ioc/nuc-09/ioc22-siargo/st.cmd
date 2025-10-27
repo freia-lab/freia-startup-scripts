f5fb6bf0ae2a0bfabe6890a16a5e2cb64523f51f
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2025-10-27 15:07:44 +0100

    Added TIME_OUT macro to siargo ioc (it didn't help to avoid modbus time-outs).

--- e3ioc/nuc-09/ioc22-siargo/st.cmd
+++ e3ioc/nuc-09/ioc22-siargo/st.cmd
@@ -3,7 +3,7 @@ require recsync
 require iocstats
 
 iocshLoad ("$(siargo_DIR)siargo-eth-ctrl.iocsh", "ASYN_PORT_NAME=PORT1,IP_ADDR=192.168.10.38,TOP=/opt/epics/autosave")
-iocshLoad ("$(siargo_DIR)siargo-eth-ctrl.iocsh", "ASYN_PORT_NAME=PORT2,IP_ADDR=130.238.22.3,TOP=/opt/epics/autosave")
+iocshLoad ("$(siargo_DIR)siargo-eth-ctrl.iocsh", "ASYN_PORT_NAME=PORT2,IP_ADDR=130.238.22.3,TIME_OUT=4200,TOP=/opt/epics/autosave")
 
 iocshLoad ("$(siargo_DIR)siargo-meter.iocsh", "ASYN_PORT_NAME=PORT1,MODBUS_ADR=1,PREFIX=Env-Gas:,DEV_NAME=MFM-FREIA,ASLO=0.001,SCALE=1")
 iocshLoad ("$(siargo_DIR)siargo-meter.iocsh", "ASYN_PORT_NAME=PORT2,MODBUS_ADR=1,PREFIX=Env-Gas:,DEV_NAME=MFM-Hus3,ASLO=0.00006,SCALE=0.001")
