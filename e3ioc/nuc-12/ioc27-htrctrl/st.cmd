fb50d54fe1b06310e268d38bb24bc6b6f63b8af9
Marek Jacewicz <marek.jacewicz@physics.uu.se>
2025-10-29 13:07:41 +0100

    ioc24-hmp removed from nuc-12. HMP4040 PSU control is now included in ioc27-heaterctrl

--- e3ioc/nuc-12/ioc27-htrctrl/st.cmd
+++ e3ioc/nuc-12/ioc27-htrctrl/st.cmd
@@ -5,16 +5,24 @@ require iocstats
 
 addScan(60)
 addScan(30)
-
 epicsEnvSet("IOCNAME", "ioc27-htrctrl")
 epicsEnvSet("IP", "192.168.10.44")
 epicsEnvSet("P", "CstatV-AC:")
+epicsEnvSet("P_HMP1", "CstatV-AC1:")
+epicsEnvSet("P_HMP2", "CstatV-AC2:")
 epicsEnvSet("TOP", "/opt/epics/autosave")
 
-iocshLoad("$(daq6510_DIR)daq6510_init.iocsh", "ASYN_PORT=DAQ_PORT,IP=192.168.10.44,P=CstatV-AC:")
+drvAsynIPPortConfigure("PORT1","192.168.10.50:5025",0,0,0)
+drvAsynIPPortConfigure("PORT2","192.168.10.45:5025",0,0,0)
 
-dbLoadRecords("asynRecord.db","P=${IOCNAME},R=:asynRec,PORT=$(ASYN_PORT=DAQ_PORT),ADDR='0',IMAX='1024',OMAX='256'")
-dbLoadRecords("heaterctrl.db", "P=CstatV-AC:, DEV=DAQ6510, PORT=DAQ_PORT")
+dbLoadRecords("hmp40x0.db", "PORT=PORT1,P=$(P_HMP1),DEV=HMP4040")
+dbLoadRecords("hmp40x0.db", "PORT=PORT2,P=$(P_HMP2),DEV=HMP4040")
+
+iocshLoad("$(daq6510_DIR)daq6510_init.iocsh", "ASYN_PORT=DAQ_PORT,IP=$(IP),P=$(P)")
+
+dbLoadRecords("asynRecord.db", "P=${IOCNAME},R=:asynRec,PORT=$(ASYN_PORT=DAQ_PORT),ADDR='0',IMAX='1024',OMAX='256'")
+dbLoadRecords("heaterctrl.db", "P=$(P), DEV=DAQ6510, PORT=DAQ_PORT")
+dbLoadRecords("heaterctrl-aliases.db", "P=$(P), PSU1=$(P_HMP1), PSU2=$(P_HMP2), DEV1=DAQ6510, DEV2=HMP4040")
 
 iocshLoad("$(autosave_DIR)/autosave.iocsh", "AS_TOP=$(TOP),IOCNAME=$(IOCNAME),IOCDIR=$(IOCDIR=daq6510)")
 
