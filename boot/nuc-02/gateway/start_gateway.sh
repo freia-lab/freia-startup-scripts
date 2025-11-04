44448c6bdce20a5071f9f33a67606580a4f0d76e
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2025-11-04 10:40:31 +0100

    BUG FIX: modified ca gateway start-up script: removed -uid and -gid arguments causing crash of the gateway program on Ubuntu 22.04.

--- boot/nuc-02/gateway/start_gateway.sh
+++ boot/nuc-02/gateway/start_gateway.sh
@@ -11,14 +11,15 @@ gid=995
 export PATH=/epics/base-7.0.7/require/5.0.0/bin:/epics/base-7.0.7/bin/linux-x86_64:/usr/sbin:/usr/bin:/sbin:/bin
 export EPICS_HOST_ARCH=linux-x86_64
 export EPICS_BASE=/epics/base-7.0.7
-export LD_LIBRARY_PATH=/epics/base-7.0.7/lib/linux-x86_64
+export LD_LIBRARY_PATH=/epics/base-7.0.7/extensions/pcas-e075fd450/lib/linux-x86_64:/epics/base-7.0.7/lib/linux-x86_64
  
 start()
 {
 	echo "Starting CA gateway in $gwhome"
 	[ ! -d $gwhome ] && mkdir $gwhome
 	chown -R $uid:$gid $gwhome
-	EPICS_CA_MAX_ARRAY_BYTES=20000000 gateway -home $gwhome -log gateway.log -prefix GW-$host -cip 192.168.10.255 -sip 130.238.200.140 -signore $host_ip -archive -no_cache -pvlist /opt/startup/boot/$host/gateway/GATEWAY.pvlist -access /opt/startup/boot/$host/gateway/GATEWAY.access -uid $uid -gid $gid -server
+#	EPICS_CA_MAX_ARRAY_BYTES=20000000 gateway -home $gwhome -log gateway.log -prefix GW-$host -cip 192.168.10.255 -sip 130.238.200.140 -signore $host_ip -archive -no_cache -pvlist /opt/startup/boot/$host/gateway/GATEWAY.pvlist -access /opt/startup/boot/$host/gateway/GATEWAY.access -uid $uid -gid $gid -server
+	EPICS_CA_MAX_ARRAY_BYTES=20000000 gateway -home $gwhome -log gateway.log -prefix GW-$host -cip 192.168.10.255 -sip 130.238.200.140 -signore $host_ip -archive -no_cache -pvlist /opt/startup/boot/$host/gateway/GATEWAY.pvlist -access /opt/startup/boot/$host/gateway/GATEWAY.access -server
 }
 
 stop()
