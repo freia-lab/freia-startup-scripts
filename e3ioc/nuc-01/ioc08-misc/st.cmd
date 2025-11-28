a5bf28e110b05ba9cfa209f47dd82e153e4a89f1
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2025-11-28 10:54:15 +0100

    Added afterInit("var streamErrorDeadTime 60") to misc IOC.

--- e3ioc/nuc-01/ioc08-misc/st.cmd
+++ e3ioc/nuc-01/ioc08-misc/st.cmd
@@ -13,3 +13,5 @@ dbLoadRecords("$(linstat_DB)linStatProc.db" ,"IOC=$(IOCNAME)")
 iocshLoad("$(miscfreia_DIR)/miscfreia.iocsh",  "TOP=/opt/epics/autosave,IP_ADDR=192.168.1.103,IP_ADDR2=192.168.1.59:1137,IP_ADDR3=192.168.1.58:1137,IP_ADDR4=192.168.10.188:1137")
 iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=$(IOCNAME)")
 
+afterInit("var streamErrorDeadTime 60")
+
