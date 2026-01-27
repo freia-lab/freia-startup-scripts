9dfd3be654d0fa89133e0b82ef747b5b56926355
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2026-01-27 13:06:34 +0100

    Added starting autosave and loading asyn record due to the changes in siargo module 1.1.5+0.

--- e3ioc/nuc-09/ioc22-siargo/st.cmd
+++ e3ioc/nuc-09/ioc22-siargo/st.cmd
@@ -3,8 +3,10 @@ require recsync
 require iocstats
 require linstat
 
-iocshLoad ("$(siargo_DIR)siargo-eth-ctrl.iocsh", "ASYN_PORT_NAME=PORT1,IP_ADDR=192.168.10.38,TOP=/opt/epics/autosave")
-iocshLoad ("$(siargo_DIR)siargo-eth-ctrl.iocsh", "ASYN_PORT_NAME=PORT2,IP_ADDR=130.238.22.3,TIME_OUT=4200,TOP=/opt/epics/autosave")
+epicsEnvSet("IOCDIR", "siargo")
+
+iocshLoad ("$(siargo_DIR)siargo-eth-ctrl.iocsh", "ASYN_PORT_NAME=PORT1,IP_ADDR=192.168.10.38")
+iocshLoad ("$(siargo_DIR)siargo-eth-ctrl.iocsh", "ASYN_PORT_NAME=PORT2,IP_ADDR=130.238.22.3,TIME_OUT=4200")
 
 iocshLoad ("$(siargo_DIR)siargo-meter.iocsh", "ASYN_PORT_NAME=PORT1,MODBUS_ADR=1,PREFIX=Env-Gas:,DEV_NAME=MFM-FREIA,ASLO=0.001,SCALE=1")
 iocshLoad ("$(siargo_DIR)siargo-meter.iocsh", "ASYN_PORT_NAME=PORT2,MODBUS_ADR=1,PREFIX=Env-Gas:,DEV_NAME=MFM-Hus3,ASLO=0.00006,SCALE=0.001")
@@ -12,6 +14,10 @@ iocshLoad ("$(siargo_DIR)siargo-meter.iocsh", "ASYN_PORT_NAME=PORT2,MODBUS_ADR=1
 # iocStats database
 dbLoadRecords("$(iocstats_DB)/iocAdminSoft-freia-proc-limited.db","IOC=ioc22-siargo")
 dbLoadRecords("$(linstat_DB)linStatProc.db" ,"IOC=$(IOCNAME)")
+
+#################################################
+iocshLoad("$(autosave_DIR)autosave.iocsh", "AS_TOP=/opt/epics/autosave,IOCNAME=$(IOCNAME)")
+#################################################
 
 # Start recsync client
 iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=ioc22-siargo")
